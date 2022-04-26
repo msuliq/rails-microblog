578203da26e9a58be1108c7b5285ebf1b6bb7983
Suleyman Musayev <slmusayev@gmail.com>
2022-04-26 20:43:27 +0500

    update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+#enable reporting tool for minitest
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
