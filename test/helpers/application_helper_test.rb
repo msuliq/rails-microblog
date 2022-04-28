f0b022080f882f136e37b806cead9686434028bd
Suleyman Musayev <slmusayev@gmail.com>
2022-04-29 00:06:26 +0500

    add test for application helper

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+#Application helper is included in test helper
+#this test is designed to ensure proper functioning of full_title method
+
+require 'test_helper'
+class ApplicationHelperTest < ActionView::TestCase
+    test "full title helper" do
+        assert_equal full_title, "Sample microblog app"
+        assert_equal full_title("Help"), "Help | Sample microblog app"
+    end
+end
