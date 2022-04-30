f7253993a0c5d8e4c8e693d00bcc4d33a91a1019
Suleyman Musayev <slmusayev@gmail.com>
2022-04-30 21:00:06 +0500

    add validation for min password length to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 50 }
     #custom regex to check email validity
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     
     has_secure_password
+    validates :password, length: { minimum: 6}
 
 end
