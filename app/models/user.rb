0e2c186d330c11092a8c489d7380b1f4588feff4
Suleyman Musayev <slmusayev@gmail.com>
2022-04-30 20:41:45 +0500

    add name and email validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true, length: { maximum: 50 }
+    #custom regex to check email validity
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
 end
