3874112f332bd5ca045af97c0b6db96841489547
Suleyman Musayev <slmusayev@gmail.com>
2022-05-02 14:06:31 +0500

    update sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session] [:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    #login and redirect to user profile page
+    else
+     #flash anerror message
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
