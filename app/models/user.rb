76bc018ac39b8c39b974cfbfd69003f89aade604
Suleyman Musayev <slmusayev@gmail.com>
2022-05-02 18:49:54 +0500

    fix issue with logout in different tabs and add model and integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,12 @@ class User < ActiveRecord::Base
 
     # Returns true is token matches digest
     def authenticated?(remember_token)
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets the user
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
