e170ef28887c796a6674b8ee8f392194b0a41ab4
Suleyman Musayev <slmusayev@gmail.com>
2022-05-03 14:14:38 +0500

    add profile update method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,18 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params:[:id])
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Process successful update
+    else
+      render 'edit'
+    end
+  end
+    
   private
   
     def user_params
