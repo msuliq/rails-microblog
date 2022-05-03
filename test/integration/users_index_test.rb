51cc42394a4e499c890769de4581df6afd8b2fa2
Suleyman Musayev <slmusayev@gmail.com>
2022-05-03 15:51:12 +0500

    add admin tests to user controller and user index integration tests

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,9 +3,12 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
  
   def setup
-    @user = users(:michael)
+    @user = users(:dwight)
+    @admin = users(:michael) # Michael is set admin in fixtures
+    @non_admin = users(:pam)
   end
 
+  # Test involves only users with no difference whether admin or not
   test "index including pagination" do
     log_in_as(@user)
     get users_path
@@ -14,6 +17,32 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  # Following tests differentiate admins and non-admins
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete',
+        method: :delete
+      end
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+  
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 
 end
