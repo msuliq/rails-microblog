189f9834fefecba481376f76a6a3ad501d993dbe
Suleyman Musayev <slmusayev@gmail.com>
2022-05-05 10:39:17 +0500

    add methods for password reset to user model and password resets controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-    # Adds accessors to create activation token and to remember token in cookies
-    attr_accessor :remember_token, :activation_token
+    # Adds accessors to create activation token,
+    # remember and reset password token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
@@ -36,6 +37,18 @@ class User < ActiveRecord::Base
     # Sends email with link to activation page
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # Sets attributes for password reset
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+    
+    # Sends email with link to the password reset form
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
     end
 
     # Remembers user in db for permanent session
