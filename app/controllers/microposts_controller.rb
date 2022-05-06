1787d82eee3ee60d2a27cfbdfa3e5d67b47c9266
Suleyman Musayev <slmusayev@gmail.com>
2022-05-06 17:14:51 +0500

    add filter for logged in user to microposts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,11 @@
 class MicropostsController < ApplicationController
+    # Logged in user method is defined in application controller
+    before_action :logged_in_user, only: [:create, :destroy]
+
+    def create
+    end
+
+    def destroy
+    end
+    
 end
