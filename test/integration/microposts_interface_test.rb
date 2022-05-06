62f3a048e653dfe1dce6932ddf164d63bd8462f7
Suleyman Musayev <slmusayev@gmail.com>
2022-05-06 18:09:57 +0500

    add micropost interface integration test

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+
+    # Wrong data in the form
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, micropost: { content: "" }
+    end
+
+    assert_select 'div#error_explanation'
+    
+    # Valid data in the form
+    content = "This micropost really ties the room together"
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: { content: content }
+    end
+    
+    assert_redirected_to root_url 
+    follow_redirect!
+    assert_match content, response.body
+    
+    # Deletion of a post
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    
+    # Redirect to another user's profile
+    get user_path(users(:dwight))
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+end
