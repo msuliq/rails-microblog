9d1a0e36c36b27a68b11b6565005ff2e54f885d9
Suleyman Musayev <slmusayev@gmail.com>
2022-05-06 11:53:20 +0500

    add dependent destroy into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
     # Adds accessors to create activation token,
     # remember and reset password token
     attr_accessor :remember_token, :activation_token, :reset_token
