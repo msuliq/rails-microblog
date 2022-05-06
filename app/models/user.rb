d8ae6a02ee1529aa204ac29b3d5a20856457f758
Suleyman Musayev <slmusayev@gmail.com>
2022-05-06 11:46:37 +0500

    add has many posts relation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :microposts
     # Adds accessors to create activation token,
     # remember and reset password token
     attr_accessor :remember_token, :activation_token, :reset_token
