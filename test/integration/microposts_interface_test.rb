610f9db44ff015cba514dba32d1573589bcb7a23
Suleyman Musayev <slmusayev@gmail.com>
2022-05-07 13:18:19 +0500

    add micropost counter test to interface integration test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -40,4 +40,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User has no posts
+    other_user = users(:angela)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    num = other_user.microposts.count
+    assert_match "#{num} micropost".pluralize(num), response.body
+  end
+
 end
