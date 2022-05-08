68c381e79e61b84d303d4fe2933156ec38d6aa0c
Suleyman Musayev <slmusayev@gmail.com>
2022-05-08 12:05:00 +0500

    add quote signs to microposts interface test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,7 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_select 'input[type=file]'
+    assert_select 'input[type="file"]'
 
     # Wrong data in the form
     assert_no_difference 'Micropost.count' do
