94250413d80acc50c93a7aac5357ed0dfd500148
Suleyman Musayev <slmusayev@gmail.com>
2022-05-21 17:42:25 +0500

    remove deprecation warning for SQL3 adapter boolean as integer

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,21 @@ module RailsMicroblog
 
     # Enable authentification token for remote forms
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    
+    # Leaving `ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer`
+    # set to false is deprecated. SQLite databases have used 't' and 'f' to serialize
+    # boolean values and must have old data converted to 1 and 0 (its native boolean
+    # serialization) before setting this flag to true. Conversion can be accomplished
+    # by setting up a rake task which runs
+
+    #   ExampleModel.where("boolean_column = 't'").update_all(boolean_column: 1)
+    #   ExampleModel.where("boolean_column = 'f'").update_all(boolean_column: 0)
+
+    # for all models and all boolean columns, after which the flag must be set to
+    # true by adding the following to your application.rb file:
+
+    #   Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+    # (called from <top (required)> at /Users/Suleyman/Projects/rails_microblog/config/environment.rb:8)
+    config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
