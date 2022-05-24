67fe40dea991e5ee9154b2b98b86dd78d0db5afd
Suleyman Musayev <slmusayev@gmail.com>
2022-05-24 18:36:08 +0500

    fix controller tests for passing hash thru params

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,27 +13,27 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should redirect edit when not logged in" do
-    get :edit, id: @user
+    get :edit, params: { id: @user }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
   
   test "should redirect update when not logged in" do
-    patch :update, id: @user, params: { user: { name: @user.name, email: @user.email } }
+    patch :update, params: { id: @user, user: { name: @user.name, email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
-    get :edit, id: @user
+    get :edit, params: { id: @user }
     assert flash.empty?
     assert_redirected_to root_url
   end
   
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch :update, id: @user, params: { user: { name: @user.name, email: @user.email } }
+    patch :update, params: { id: @user, user: { name: @user.name, email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
   end
@@ -41,7 +41,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
-    patch :update, id: @other_user, params: { user: { password: "",
+    patch :update, params: { id: @other_user, user: { password: "",
                                                       password_confirmation: "",
                                                       admin: true } }
     assert_not @other_user.reload.admin?
@@ -54,7 +54,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
-      delete :destroy, id: @user
+      delete :destroy, params: { id: @user }
     end
     
     assert_redirected_to login_url
@@ -63,19 +63,19 @@ class UsersControllerTest < ActionController::TestCase
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
-      delete :destroy, id: @user
+      delete :destroy, params: { id: @user }
     end
     
     assert_redirected_to root_url
   end
 
   test "should redirect following when not logged in" do
-    get :following, id: @user
+    get :following, params: { id: @user }
     assert_redirected_to login_url
   end
   
   test "should redirect followers when not logged in" do
-    get :followers, id: @user
+    get :followers, params: { id: @user }
     assert_redirected_to login_url
   end
 
