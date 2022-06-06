48cbb26667657fe0c24b617682a0dcd806c1188b
Suleyman Musayev <slmusayev@gmail.com>
2022-06-06 22:31:35 +0500

    fix deprecated faker method in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ User.create!(name: "Example User",
 # Microposts
 users = User.order(:created_at).take(6)
     50.times do
-        content = Faker::Lorem.sentence(5)
+        content = Faker::Lorem.sentence(word_count: 5)
         users.each { |user| user.microposts.create!(content: content) }
     end
 
