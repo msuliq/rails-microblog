5f5747f623efc3c4281c46dd85fbcfad0a0be157
Suleyman Musayev <slmusayev@gmail.com>
2022-06-17 00:39:57 +0500

    set default image variant to vips

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,3 @@ require File.expand_path('application', __dir__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Initialize carrierwave gem
-require 'carrierwave/orm/activerecord'
