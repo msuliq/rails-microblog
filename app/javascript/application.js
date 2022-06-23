0fe9b9aefd42edbdec45d75983f51f0ec184b0ca
Suleyman Musayev <slmusayev@gmail.com>
2022-06-23 17:03:40 +0500

    add esbuild install via bundling

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Entry point for the build script in your package.json
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+
+import "bootstrap"
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
