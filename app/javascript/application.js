3eb5b3839287a6dd4e14ae142bd873e1737c0d15
Suleyman Musayev <slmusayev@gmail.com>
2022-06-23 17:04:20 +0500

    update application.js for esbuild

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
-import "bootstrap"
+import 'bootstrap'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
 
