0b1a54790fc6f88ef071671ee17aa3e7db23fa98
Suleyman Musayev <slmusayev@gmail.com>
2022-06-27 22:47:05 +0500

    add sendgrid confg to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -89,8 +85,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
+  :port => '465',
+  :authentication => :SSL,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
