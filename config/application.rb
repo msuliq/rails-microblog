c22803fbd97e1934283bce2b0a1fc640061bbe39
Suleyman Musayev <slmusayev@gmail.com>
2022-06-28 10:15:54 +0500

    update in seeds and application.rb for reading of local env variables

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,16 @@ module RailsMicroblog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Configuration for loading local_env.yml file to read env variables
+    # http://railsapps.github.io/rails-environment-variables.html
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
