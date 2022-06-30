9a2c5a32c0e2801a633549329f75bf3a87698060
Suleyman Musayev <slmusayev@gmail.com>
2022-06-30 23:24:05 +0500

    update application.js to enable dropdown menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,10 @@
 // Entry point for the build script in your package.json
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 
-import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
 
-Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+import * as bootstrap from "bootstrap"
