ab85d84a93f307cc5576028275fcdbb48840b04c
Suleyman Musayev <slmusayev@gmail.com>
2022-07-01 10:44:47 +0500

    update tests to replace will_paginate with pagy

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,7 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test 'micropost interface' do
     log_in_as(@user)
     get root_path
-    assert_select 'div.pagination'
+    assert_select 'ul.pagination'
     assert_select 'input[type="file"]'
 
     # Wrong data in the form
@@ -33,7 +33,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Deletion of a post
     assert_select 'a', text: 'delete'
-    first_micropost = @user.microposts.paginate(page: 1).first
+    first_micropost = pagy(@user.microposts, page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -14,8 +14,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    assert_select 'ul.pagination'
+    pagy(User.class, page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
@@ -25,8 +25,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    assert_select 'ul.pagination'
+    first_page_of_users = pagy(User.class, page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
