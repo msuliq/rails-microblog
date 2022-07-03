32aeefd7b96fc943844acb488705dc8789b089c5
Suleyman Musayev <slmusayev@gmail.com>
2022-07-04 00:47:29 +0500

    add pagy backend into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # Sessions helper add support for login, logout and cookies
-  include SessionsHelper
-  include Turbo::Redirection
   include Pagy::Backend
-
+  include Turbo::Redirection
+  include SessionsHelper
+ 
   private
 
   # Used in users and microposts controllers
