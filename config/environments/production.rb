5bfffa4baac7604ecfacac8e09ec97402cad3c5d
Suleyman Musayev <slmusayev@gmail.com>
2022-07-04 22:48:35 +0500

    modify sendgrid mailer settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: '465',
-    authentication: :SSL,
+    port: '587',
+    authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'rails-microblogg.herokuapp.com',
     enable_starttls_auto: true
   }
 end
