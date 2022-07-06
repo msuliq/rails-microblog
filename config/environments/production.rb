4deadda1aed12e8d35d63c95eaab520540f8c4ff
Suleyman Musayev <slmusayev@gmail.com>
2022-07-06 14:54:21 +0500

    add changes to host settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.delivery_method = :smtp
 
-  host = 'https://mail.ee'
+  host = 'https://rails-microblogg.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
